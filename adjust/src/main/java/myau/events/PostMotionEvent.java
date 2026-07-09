1553236c2f821220e01484f1b828d27bcc138fc4
Mornly <1038444598@qq.com>
2026-07-09 10:45:16 +0800

    QAQ

--- adjust/src/main/java/myau/events/PostMotionEvent.java
+++
@@ -1,7 +0,0 @@
-package myau.events;
-
-import myau.event.events.Event;
-
-public class PostMotionEvent implements Event {
-    // 空事件，用于标记运动后的钩子
-}
--- adjust/src/main/java/myau/font/CFontRenderer.java
+++
@@ -1,7 +0,0 @@
-package myau.font;
-
-public class CFontRenderer extends UFontRenderer {
-    public CFontRenderer(String name, int size) {
-        super(name, size);
-    }
-}
